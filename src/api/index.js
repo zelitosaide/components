175ac3dbdb056be83dad34826983c69ef5c2e165
zelitosaide <zelitosaide83@gmail.com>
2023-02-14 22:06:19 +0200

    Task App project

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 export const baseUrl = "http://localhost:5000/v1/";
 
 export async function addTask(task) {
-  const response = await fetch(baseUrl + "tasks/" + task._id, {
-    method: "PATCH",
+  const response = await fetch(baseUrl + "tasks", {
+    method: "POST",
     body: JSON.stringify(task),
     headers: {
       "Content-type": "application/json; charset=UTF-8",
