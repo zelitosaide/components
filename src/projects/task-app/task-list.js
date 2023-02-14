8225114959d9677a6f8395af656307f7de0d3dc6
zelitosaide <zelitosaide83@gmail.com>
2023-02-14 13:38:12 +0200

    Task App project

--- src/projects/task-app/task-list.js
+++ src/projects/task-app/task-list.js
@@ -1,1 +1,19 @@
-export default function TaskList() {}
+import Task from "./task";
+
+export default function TaskList({ tasks, onChangeTask, onDeleteTask }) {
+  return (
+    <ul>
+      {tasks.map(function (task) {
+        return (
+          <li key={task._id}>
+            <Task
+              task={task}
+              onChange={onChangeTask}
+              onDelete={onDeleteTask}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
---
+++ src/projects/task-app/task.js
@@ -0,0 +1,1 @@
+export default function Task({ task, onChange, onDelete }) {}
