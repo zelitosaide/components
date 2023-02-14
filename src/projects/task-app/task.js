5cb9710b8e0e9602bf0d58493fd2862d060a6c12
zelitosaide <zelitosaide83@gmail.com>
2023-02-14 13:46:57 +0200

    Task App project

--- src/projects/task-app/task.js
+++ src/projects/task-app/task.js
@@ -1,1 +1,29 @@
-export default function Task({ task, onChange, onDelete }) {}
+import { useState } from "react";
+
+export default function Task({ task, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let taskContent;
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={task.done}
+        onChange={function (e) {
+          onChange({
+            ...task,
+            done: e.target.checked,
+          });
+        }}
+      />
+      {taskContent}
+      <button
+        onClick={function () {
+          onDelete(task._id);
+        }}
+      >
+        Delete
+      </button>
+    </label>
+  );
+}
