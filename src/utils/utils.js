454c0837bfe997fd0c47f39b9b55db13355dc976
zelitosaide <zelitosaide83@gmail.com>
2023-02-16 13:10:14 +0200

    Component

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,4 +2,9 @@ export function formatDate(date) {
   return date.toISOString().split("T")[0];
 }
 
-export function formatTime(date) {}
+export function formatTime(date) {
+  new Intl.DateTimeFormat("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+  }).format(date);
+}
