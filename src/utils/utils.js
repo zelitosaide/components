476cfba4e3df2df9ec86d2a930ee8057e12830a3
zelitosaide <zelitosaide83@gmail.com>
2023-02-17 05:31:41 +0200

    Component

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,14 +1,14 @@
-export function formatDate(date) {
+export function formatDateForInputField(date) {
   return date.toISOString().slice(0, 10);
 }
 
-export function formatTime(date) {
+export function formatTimeForInputField(date) {
   return new Intl.DateTimeFormat("pt-PT", {
     hour: "2-digit",
     minute: "2-digit",
   }).format(date);
 }
 
-export function formatDateIntl(date) {
+export function formatDate(date) {
   return new Intl.DateTimeFormat("pt-PT", { dateStyle: "full" }).format(date);
 }
