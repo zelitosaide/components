45e983344c9a8d80924c20ecaf22a3b9b222dc2a
zelitosaide <zelitosaide83@gmail.com>
2023-02-21 21:40:52 +0200

    TaskApp

--- src/components/core/button.js
+++ src/components/core/button.js
@@ -4,6 +4,10 @@ export function Button({ children, onClick, style, disabled }) {
       style={{
         ...style,
         backgroundColor: "hsl(119, 72%, 35%)",
+        border: "none",
+        color: "white",
+        padding: "5px 8px",
+        borderRadius: 5,
       }}
       onClick={function (e) {
         e.stopPropagation();
