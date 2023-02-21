47a84ac80967678391517eb311f9e50bcde2c278
zelitosaide <zelitosaide83@gmail.com>
2023-02-21 22:29:03 +0200

    TaskApp

--- src/projects/task-app/task-list.js
+++ src/projects/task-app/task-list.js
@@ -2,14 +2,13 @@ import Task from "./task";
 
 export default function TaskList({ tasks, title }) {
   return (
-    <div>
+    <div style={{ backgroundColor: "#1D1E22" }}>
       <h4>{title}</h4>
       <ul
         style={{
           listStyleType: "none",
           margin: 0,
           padding: 0,
-          backgroundColor: "#1D1E22",
         }}
       >
         {tasks.map(function (task) {
